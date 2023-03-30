5ed4593bf9db106c077c18fc94913066216ca85d
daniel <dvds1987@gmail.com>
2023-03-30 11:18:33 +0200

    Setup jsconfig paths

--- src/pages/auth/login/Login.js
+++ src/pages/auth/login/Login.js
@@ -35,7 +35,7 @@ const Login = () => {
       setLoading(false);
       setHasError(true);
       setAlertType("alert-error");
-      setErrorMessage(error?.response?.data.message);
+      setErrorMessage(error?.response?.data?.message);
     }
   };
 
