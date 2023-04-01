3ef6fcece3e3e966c2c327184d8a06d9a5b83dd2
daniel <dvds1987@gmail.com>
2023-04-01 11:27:29 +0200

    Header component with avatar

--- src/pages/social/Social.js
+++ src/pages/social/Social.js
@@ -1,15 +1,16 @@
+import { Outlet } from "react-router-dom";
 import "@pages/social/Social.scss";
-import Streams from "./streams/streams";
+import Header from "@components/header/Header";
+// import Sidebar from "@components/sidebar/Sidebar";
 
 const Social = () => {
   return (
     <>
+      <Header />
       <div className="dashboard">
-        <div className="dashboard-sidebar">
-          <h1>sidebar</h1>
-        </div>
+        <div className="dashboard-sidebar">{/* <Sidebar /> */}</div>
         <div className="dashboard-content">
-          <Streams />
+          <Outlet />
         </div>
       </div>
     </>
