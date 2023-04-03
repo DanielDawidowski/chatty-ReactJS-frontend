462359d88c163a519e8248c260d83a182d52b857
daniel <dvds1987@gmail.com>
2023-04-03 19:05:57 +0200

    Notification component unit test

--- src/mocks/server.js
+++ src/mocks/server.js
@@ -1,5 +1,7 @@
 import { setupServer } from "msw/node";
 import { authHandlers } from "@mocks/handlers/auth";
+import { userHandlers } from "./handlers/user";
+import { notificationHandlers } from "./handlers/notifications";
 
 // Setup requests interception using the given handlers
-export const server = setupServer(...authHandlers);
+export const server = setupServer(...authHandlers, ...userHandlers, ...notificationHandlers);
