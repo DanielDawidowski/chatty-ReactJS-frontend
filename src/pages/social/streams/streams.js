da544071995205e52a970f592d66067d18a4cdbc
daniel <dvds1987@gmail.com>
2023-04-04 11:13:03 +0200

    Post form component

--- src/pages/social/streams/streams.js
+++ src/pages/social/streams/streams.js
@@ -5,6 +5,7 @@ import "@pages/social/streams/Streams.scss";
 import Suggestions from "@components/suggestions/Suggestions";
 import { getUserSuggestions } from "@redux/api/suggestions";
 import useEffectOnce from "@hooks/useEffectOnce";
+import PostForm from "@components/posts/post-form/PostForm";
 
 function Streams() {
   const bodyRef = useRef(null);
@@ -19,7 +20,7 @@ function Streams() {
     <div className="streams" data-testid="streams">
       <div className="streams-content">
         <div className="streams-post" ref={bodyRef} style={{ backgroundColor: "white" }}>
-          <div>Post Form</div>
+          <PostForm />
           <div>Post Items</div>
           <div ref={bottomLineRef} style={{ marginBottom: "50px", height: "50px" }}></div>
         </div>
