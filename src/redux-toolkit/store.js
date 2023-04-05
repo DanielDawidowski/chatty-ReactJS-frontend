2b9959f7968aa0bb39dc36c26d314bd5b045bffa
daniel <dvds1987@gmail.com>
2023-04-05 09:15:55 +0200

    Post modal reducer & wrapper

--- src/redux-toolkit/store.js
+++ src/redux-toolkit/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "@redux/reducers/user/user.reducer";
 import suggestionsReducer from "@redux/reducers/suggestions/suggestions.reducer";
 import notificationsReducer from "@redux/reducers/notifications/notifications.reducer";
+import modalReducer from "./reducers/modal/modal.reducer";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     suggestions: suggestionsReducer,
-    notifications: notificationsReducer
+    notifications: notificationsReducer,
+    modal: modalReducer
   }
 });
