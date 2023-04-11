4ec74c852b94e74379260df31278781e509602a0
daniel <dvds1987@gmail.com>
2023-04-11 10:44:07 +0200

    Add post reaction

--- src/services/api/post/post.service.js
+++ src/services/api/post/post.service.js
@@ -20,6 +20,23 @@ class PostService {
     const response = await axios.get(`/post/reactions/username/${username}`);
     return response;
   }
+
+  async getSinglePostReactionByUsername(postId, username) {
+    const response = await axios.get(`/post/single/reaction/username/${username}/${postId}`);
+    return response;
+  }
+
+  async addReaction(body) {
+    const response = await axios.post("/post/reaction", body);
+    return response;
+  }
+
+  async removeReaction(postId, previousReaction, postReactions) {
+    const response = await axios.delete(
+      `/post/reaction/${postId}/${previousReaction}/${JSON.stringify(postReactions)}`
+    );
+    return response;
+  }
 }
 
 export const postService = new PostService();
