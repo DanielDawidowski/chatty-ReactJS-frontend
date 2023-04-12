b7e29ccad6654d4fbef5e4e23e61104d916f3a9a
daniel <dvds1987@gmail.com>
2023-04-12 18:11:57 +0200

    People Components, Card element stats and buttons components

--- src/pages/social/people/People.js
+++ src/pages/social/people/People.js
@@ -1,8 +1,76 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./People.scss";
+import { FaCircle } from "react-icons/fa";
+import { Utils } from "@services/utils/utils.service";
+import Avatar from "@components/avatar/Avatar";
+import useInfiniteScroll from "@hooks/useInfiniteScroll";
+import CardElementStats from "@components/card-element/CardElementStats";
+import CardElementButtons from "@components/card-element/CardElementButtons";
 
 function People() {
-  return <div>People Page</div>;
+  const [users] = useState([]);
+  const [onlineUsers] = useState([]);
+  const [loading] = useState(true);
+  const bodyRef = useRef(null);
+  const bottomLineRef = useRef(null);
+
+  useInfiniteScroll(bodyRef, bottomLineRef, fetchData);
+
+  function fetchData() {}
+
+  return (
+    <div className="card-container" ref={bodyRef}>
+      <div className="people">People Page</div>
+      {users.length > 0 && (
+        <div className="card-element">
+          {users.map((data, index) => (
+            <div className="card-element-item" key={index} data-testid="card-element-item">
+              {Utils.checkIfUserIsOnline(data?.username, onlineUsers) && (
+                <div className="card-element-item-indicator">
+                  <FaCircle className="online-indicator" />
+                </div>
+              )}
+              <div className="card-element-header">
+                <div className="card-element-header-bg"></div>
+                <Avatar
+                  name={data?.username}
+                  bgColor={data?.avatarColor}
+                  textColor="#ffffff"
+                  size={120}
+                  avatarSrc={data?.profilePicture}
+                />
+                <div className="card-element-header-text">
+                  <span className="card-element-header-name">{data?.username}</span>
+                </div>
+              </div>
+              <CardElementStats
+                postsCount={data?.postsCount}
+                followersCount={data?.followersCount}
+                followingCount={data?.followingCount}
+              />
+              <CardElementButtons
+                isChecked={Utils.checkIfUserIsFollowed([], data?._id)}
+                btnTextOne="Follow"
+                btnTextTwo="Unfollow"
+                onClickBtnOne={() => {}}
+                onClickBtnTwo={() => {}}
+                onNavigateToProfile={() => {}}
+              />
+            </div>
+          ))}
+        </div>
+      )}
+      {loading && !users.length && <div className="card-element" style={{ height: "350px" }}></div>}
+
+      {!loading && !users.length && (
+        <div className="empty-page" data-testid="empty-page">
+          No user available
+        </div>
+      )}
+
+      <div ref={bottomLineRef} style={{ marginBottom: "80px", height: "50px" }}></div>
+    </div>
+  );
 }
 
 export default People;
