edd7b6b9b5b3ad8d8bdd84195e8ce442da6951c6
daniel <dvds1987@gmail.com>
2023-04-13 12:29:14 +0200

    Get user following from streams component

--- src/pages/social/streams/streams.js
+++ src/pages/social/streams/streams.js
@@ -1,41 +1,39 @@
-import React, { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-// import useInfiniteScroll from "@hooks/useInfiniteScroll";
 import "@pages/social/streams/Streams.scss";
 import Suggestions from "@components/suggestions/Suggestions";
 import { getUserSuggestions } from "@redux/api/suggestions";
 import useEffectOnce from "@hooks/useEffectOnce";
 import PostForm from "@components/posts/post-form/PostForm";
 import Posts from "@components/posts/Posts";
-import { postService } from "@services/api/post/post.service";
 import { Utils } from "@services/utils/utils.service";
+import { postService } from "@services/api/post/post.service";
 import { getPosts } from "@redux/api/posts";
 import { orderBy, uniqBy } from "lodash";
 import useInfiniteScroll from "@hooks/useInfiniteScroll";
 import { PostUtils } from "@services/utils/post-utils.service";
-import { addReactions } from "@redux/reducers/post/user-post-reaction.reducer";
 import useLocalStorage from "@hooks/useLocalStorage";
+import { addReactions } from "@redux/reducers/post/user-post-reaction.reducer";
+import { followerService } from "@services/api/followers/followers.service";
 
-function Streams() {
+const Streams = () => {
   const { allPosts } = useSelector((state) => state);
   const [posts, setPosts] = useState([]);
+  const [following, setFollowing] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [totalPostsCount, setTotalPostsCount] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
+  const [totalPostsCount, setTotalPostsCount] = useState(0);
   const bodyRef = useRef(null);
   const bottomLineRef = useRef();
-  const dispatch = useDispatch();
   let appPosts = useRef([]);
+  const dispatch = useDispatch();
   const storedUsername = useLocalStorage("username", "get");
   const [deleteSelectedPostId] = useLocalStorage("selectedPostId", "delete");
-
   useInfiniteScroll(bodyRef, bottomLineRef, fetchPostData);
-
   const PAGE_SIZE = 8;
 
   function fetchPostData() {
     let pageNum = currentPage;
-    console.log(currentPage, Math.round(totalPostsCount / PAGE_SIZE));
     if (currentPage <= Math.round(totalPostsCount / PAGE_SIZE)) {
       pageNum += 1;
       setCurrentPage(pageNum);
@@ -45,14 +43,23 @@ function Streams() {
 
   const getAllPosts = async () => {
     try {
-      const response = await postService.getAllPosts(1);
+      const response = await postService.getAllPosts(currentPage);
       if (response.data.posts.length > 0) {
         appPosts = [...posts, ...response.data.posts];
         const allPosts = uniqBy(appPosts, "_id");
-        setPosts(allPosts);
-        console.log(allPosts);
+        const orderedPosts = orderBy(allPosts, ["createdAt"], ["desc"]);
+        setPosts(orderedPosts);
       }
       setLoading(false);
+    } catch (error) {
+      Utils.dispatchNotification(error.response.data.message, "error", dispatch);
+    }
+  };
+
+  const getUserFollowing = async () => {
+    try {
+      const response = await followerService.getUserFollowing();
+      setFollowing(response.data.following);
     } catch (error) {
       Utils.dispatchNotification(error.response.data.message, "error", dispatch);
     }
@@ -68,14 +75,12 @@ function Streams() {
   };
 
   useEffectOnce(() => {
+    getUserFollowing();
     getReactionsByUsername();
     deleteSelectedPostId();
-  });
-
-  useEffect(() => {
     dispatch(getPosts());
     dispatch(getUserSuggestions());
-  }, [dispatch]);
+  });
 
   useEffect(() => {
     setLoading(allPosts?.isLoading);
@@ -91,10 +96,9 @@ function Streams() {
   return (
     <div className="streams" data-testid="streams">
       <div className="streams-content">
-        <div className="streams-post" ref={bodyRef} style={{ backgroundColor: "white" }}>
+        <div className="streams-post" ref={bodyRef}>
           <PostForm />
-          <Posts allPosts={posts} postsLoading={loading} userFollowing={[]} />
-          <div>Post Items</div>
+          <Posts allPosts={posts} postsLoading={loading} userFollowing={following} />
           <div ref={bottomLineRef} style={{ marginBottom: "50px", height: "50px" }}></div>
         </div>
         <div className="streams-suggestions">
@@ -103,6 +107,6 @@ function Streams() {
       </div>
     </div>
   );
-}
+};
 
 export default Streams;
