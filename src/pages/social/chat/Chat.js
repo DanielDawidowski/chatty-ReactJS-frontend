cc279e3909f8b1dc0fc3790ba80f000d6731a3ff
daniel <dvds1987@gmail.com>
2023-04-14 14:53:55 +0200

    Chat window component

--- src/pages/social/chat/Chat.js
+++ src/pages/social/chat/Chat.js
@@ -1,5 +1,5 @@
 import ChatList from "@components/chat/list/ChatList";
-// import ChatWindow from "@components/chat/window/ChatWindow";
+import ChatWindow from "@components/chat/window/ChatWindow";
 import useEffectOnce from "@hooks/useEffectOnce";
 import "@pages/social/chat/Chat.scss";
 import { getConversationList } from "@redux/api/chat";
@@ -20,8 +20,7 @@ const Chat = () => {
           <ChatList />
         </div>
         <div className="private-chat-wrapper-content-conversation">
-          {(selectedChatUser || chatList.length > 0) && <span>Chat Window</span>}
-          {/* {(selectedChatUser || chatList.length > 0) && <ChatWindow />} */}
+          {(selectedChatUser || chatList.length > 0) && <ChatWindow />}
           {!selectedChatUser && !chatList.length && (
             <div className="no-chat" data-testid="no-chat">
               Select or Search for users to chat with
